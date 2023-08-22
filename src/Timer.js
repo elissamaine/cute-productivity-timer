a34acab2945fca0535881571ad9b74bc4664e9c0
elissamaine <emaine97@yahoo.com>
2023-08-21 19:22:15 -0700

    added images and images folder

---
+++ src/Timer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Timer = () => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  
+
+  return (
+    <div className='timer'>
+
+    </div>
+  );
+};
+
+export default Timer;
