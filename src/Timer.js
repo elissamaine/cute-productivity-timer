f7c0f4f5ebe2c248bde237665c8d974b3c496412
Elissa Maine <emaine97@yahoo.com>
2023-08-23 18:03:46 -0700

    added pseudocode

--- src/Timer.js
+++ src/Timer.js
@@ -6,11 +6,13 @@ const Timer = () => {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
-  
+  // have buttons on the top of the page that have the pre determined times for the timer 
+  // an hour, 45min, and 15min
+  //take whichever time on the button clicked and create timer counting down from that time
 
   return (
     <div className='timer'>
-
+      
     </div>
   );
 };
